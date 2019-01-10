708bd2b235813dfba1342528b6cdbfbc435b6031
Automatic build <build@fluksaqua.com>
2019-01-10 17:53:47 +0100

    call correct dependency

--- es/hyphenateProperty.js
+++ es/hyphenateProperty.js
@@ -1,4 +1,4 @@
-import hyphenateStyleName from 'hyphenate-style-name'
+import hyphenateStyleName from 'hyphenate-style-name-esmodule-browser-export'
 
 export default function hyphenateProperty(property) {
   return hyphenateStyleName(property)
